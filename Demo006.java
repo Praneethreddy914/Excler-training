d2b6e4ac87a63004dd35f1a0f4e68e9b2fad883d
Praneethreddy914 <reddypraneeth811@gmail.com>
2025-01-03 21:15:34 +0530

    while username

---
+++ Demo006.java
@@ -0,0 +1,10 @@
+public class Demo006{
+    public static void main(String[] args){
+        String name = "praneeth";
+        String pwd = "praneeth@123";
+        while ((name == "praneeth") && (pwd == "praneeth@123")){
+            System.out.println("Hello praneeth welcome to malla reddy university");
+            break;
+        }
+    }
+}
