14b3d635b60c3ee4aef62c5f2f5e086e630ed54a
Praneethreddy914 <reddypraneeth811@gmail.com>
2025-01-21 21:41:48 +0530

    do while loop id and password

--- Demo008.java
+++ Demo008.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Demo8 {    
+public class Demo008 {    
         public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in);
     
